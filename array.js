c7aa72aff210fe2392a1f1c61bd600cadb68c295
Abdullah-Aljumah <Abdullah.Ali.elq.js@tuwaiq.edu.sa>
2021-10-17 17:09:35 +0300

    re add tasks

--- array.js
+++ array.js
@@ -1,11 +1,16 @@
+// Task One
 const addToArray = function (array, string) {
   return [array, string];
 };
+//End Task One
 
+// Task Two
 const convertToString = function (array) {
   return array.join();
 };
+//End Task Two
 
+// Task Three
 const updateValue = function (array, index, value) {
   if (index >= array.length) {
     return "there is no element to updated in this index";
@@ -14,7 +19,9 @@ const updateValue = function (array, index, value) {
     return array;
   }
 };
+//End Task Three
 
+// Task Four
 const onlyString = function (array) {
   let newArr = [];
   for (i = 0; i < array.length; i++) {
@@ -24,6 +31,9 @@ const onlyString = function (array) {
   }
   return newArr;
 };
+//End Task Four
+
+// Task Five
 const users = [
   ["Jane", "123456"],
   ["admin", "abc123"],
@@ -32,9 +42,48 @@ const login = function (username, password) {
   for (i = 0; i < users.length; i++) {
     if (username === users[i][0] && password === users[i][1]) {
       return "Login Successful";
-    } 
+    }
   }
-   return "Login Failed";
+  return "Login Failed";
+};
+//End Task Five
+
+// Task Six
+const maximumNumber = function (numbers) {
+  return Math.max(...numbers);
+};
+//End Task Six
+
+// Task Se7en
+const reversString = function (string) {
+  // split: change string to array. reverse: reverse the array. join: change the array to sting
+  return string.split("").reverse().join("");
+};
+//End Task Se7en
+
+// Extra One
+const allUsers = [
+  { username: "Jane", password: "123456" },
+  { username: "admin", password: "abc123" },
+];
+
+const createUser = function (username, password) {
+  for (let i = 0; i < allUsers.length; i++) {
+    if (username == allUsers[i].username) {
+      return "That username is not available";
+    }
+  }
+  allUsers.push({ username: `${username}`, password: `${password}` }); // Object
+  return "Registration successful";
 };
 
+// End Extra One
 
+// Extra Two
+const sliceArray = function (array, control) {
+  let newArr = [];
+  for (i = 0; i < array.length / control; i+control) {
+    newArr.push(array.splice(0, control));
+  }
+  return newArr;
+};
